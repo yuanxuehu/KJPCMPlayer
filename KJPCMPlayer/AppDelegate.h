345e997ee2e0b50d13b843a85f7726b5209dcf8f
TigerHu <yuanxuehu2016@126.com>
2024-08-22 16:51:28 +0800

    Initial Commit

---
+++ KJPCMPlayer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  KJPCMPlayer
+//
+//  Created by TigerHu on 2024/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ KJPCMPlayer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  KJPCMPlayer
+//
+//  Created by TigerHu on 2024/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
